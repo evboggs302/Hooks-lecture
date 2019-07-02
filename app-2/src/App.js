3346791bdcc32b55bee135e327c4afcaf1f81eb9
Evan Boggs Jr <evanboggs@Evans-MacBook-Pro.local>
2019-07-02 11:38:50 -0700

    hooks and custom hooks complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -5,8 +5,25 @@ import AllFriends from "./Components/AllFriends/AllFriends";
 import axios from "axios";
 import NewFriend from "./Components/NewFriend/NewFriend";
 
+const useAxios = (url, method = "get", body = null, initialData = []) => {
+  const [data, setData] = useState(initialData);
+
+  useEffect(() => {
+    axios(url, {
+      method,
+      body
+    }).then(response => {
+      setData(response.data);
+    });
+    // if the url or method changes, recall the effect
+  }, [url, method, body]);
+
+  return data;
+};
+
 function App() {
   let [myFriends, setFriends] = useState([]);
+  let axiosResult = useAxios("http://localhost:4001/api/get-friends", "get");
 
   useEffect(() => {
     axios.get("/api/get-friends").then(response => {
